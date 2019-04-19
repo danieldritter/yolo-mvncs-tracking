9ded733ba90120979607770777e2cbeeff95f4e6
Daniel Ritter <danieldritter1@gmail.com>
2019-04-19 16:51:41 -0700

    Started adding GPIO Logic to pan_tilt_controller.py

--- pan_tilt_controller.py
+++ pan_tilt_controller.py
@@ -6,30 +6,33 @@ class PanTiltController:
 	def __init__(self):
 		self.init_pins()
 		self.stop()
-
+		self.pan_pin = 18
+		self.tilt_pin = 23
+		self.im_width = 640
+		self.im_height = 480
+		self.center_point = [self.im_width/2,self.im_height/2]
 	def init_pins(self):
 		gpio.setmode(gpio.BOARD)
-		gpio.setup(8,gpio.OUT)
-
+		gpio.setup(self.pan_pin,gpio.OUT)
+		gpio.setup(self.tilt_pin,gpio.OUT)	
+		
 	def move(self):
 		self.stop()
-		gpio.output(8,True)
+		gpio.output(self.pan_pin,True)
 		time.sleep(0.3)
 		self.stop()
 		time.sleep(0.3)
 	def stop(self):
-		gpio.output(8,False)
+		gpio.output(self.pan_pin,False)
 		time.sleep(2)
 	
-        def get_direction(prev_coords,curr_coords):
-            """ 
-            Takes in the coordinates of a previous classification from YOLO and set of current coordinates 
-            for the same classification and determines which direction the pan tilt has to move to track the object.            
-            :Params: prev_coords = [x,y], curr_coords = [x,y]
-            :Returns: One of {"Left","Right","Up","Down"}
-            """
-            pass
-            
+        def get_direction(curr_coords):
+        	dir_vector = [curr_coords[0] - self.im_width,curr_coords[1]-self.im_height]
+        	return dir_vector
+
+	def adjust_position():
+		
+		off_center = dir_vector
         
         
         def close(self):
