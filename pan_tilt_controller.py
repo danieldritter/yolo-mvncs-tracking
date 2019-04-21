cd711977c7c694a399f7ffce7963e9c0b540a194
Daniel Ritter <danieldritter1@gmail.com>
2019-04-21 12:00:18 -0400

    Adding to pan_tilt_controller.py

--- pan_tilt_controller.py
+++ pan_tilt_controller.py
@@ -4,13 +4,13 @@ import time
 class PanTiltController:
 
 	def __init__(self):
-		self.init_pins()
-		self.stop()
-		self.pan_pin = 18
-		self.tilt_pin = 23
+		self.pan_pin = 23
+		self.tilt_pin = 18
 		self.im_width = 640
 		self.im_height = 480
 		self.center_point = [self.im_width/2,self.im_height/2]
+		self.init_pins()
+		self.stop()
 	def init_pins(self):
 		gpio.setmode(gpio.BOARD)
 		gpio.setup(self.pan_pin,gpio.OUT)
@@ -18,12 +18,12 @@ class PanTiltController:
 		
 	def move(self):
 		self.stop()
-		gpio.output(self.pan_pin,True)
-		time.sleep(0.3)
+		gpio.output(self.tilt_pin,True)
+		time.sleep(0.1)
 		self.stop()
 		time.sleep(0.3)
 	def stop(self):
-		gpio.output(self.pan_pin,False)
+		gpio.output(self.tilt_pin,False)
 		time.sleep(2)
 	
         def get_direction(curr_coords):
